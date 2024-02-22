99c7532e97ef709ef0c9b95c7809ffa8bd1f8af8
Ridah Shoorbajee <130719023+Redzops@users.noreply.github.com>
2024-02-21 21:57:48 -0500

    Main branch
    adding files to main branch

---
+++ Fuel Calculation.py
@@ -0,0 +1,9 @@
+# Step 1: Assign Variables
+speed_knots = 120  # Example: cruising speed in knots
+time_hours = 2     # Example: duration of the flight in hours
+
+# Step 2: Calculate Distance
+distance_nm = speed_knots * time_hours
+
+# Step 3: Display Result
+print("The total distance traveled during the flight is:", distance_nm, "nautical miles")
